ee4b18ff20c26dfa574c9781b278f105506f8e82
CAMILA OMAE <milaomae@gmail.com>
2017-10-02 22:00:55 -0300

    precisa arrumar FazerLeitura

--- PaintCSharp/Grafico/Grafico/Form1.cs
+++ PaintCSharp/Grafico/Grafico/Form1.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
-using System.Drawing;
 
 
 namespace Grafico
 {
     public partial class frmGrafico : Form
     {
+        bool esperaPonto = false;
+        bool esperaInicioReta = false;
+        bool esperaFimReta = false;
         private ListaSimples<Ponto> figuras = new ListaSimples<Ponto>();
+        private static Ponto p1 = new Ponto(0, 0, Color.Black);
+
+        Color corAtual = Color.Black;
+	
+        private void limparEsperas()
+        {
+            esperaPonto = false;
+            esperaInicioReta = false;
+            esperaFimReta = false;
+        }
+
         public frmGrafico()
         {
             InitializeComponent();
@@ -34,78 +47,116 @@ namespace Grafico
 
         private void btnAbrir_Click(object sender, EventArgs e)
         {
-            if(dlgAbrir.ShowDialog() == DialogResult.OK)
-            {
-                //abertura do arquivo, sua leitura e armazenamento
-                // de figuras na lista ligada
-                StreamReader arquivo = new StreamReader(dlgAbrir.FileName);
-                string linha = "";
-
-                while (!arquivo.EndOfStream)
+            //Está dando erro!!!
+            if (dlgAbrir.ShowDialog() == DialogResult.OK)
+                try
                 {
-                    linha = arquivo.ReadLine();
-                    if(linha != null)
+                    StreamReader arqFiguras = new StreamReader(dlgAbrir.FileName);
+                    String linha = arqFiguras.ReadLine();
+                    Double xInfEsq = Convert.ToDouble(linha.Substring(0, 5).Trim());
+                    Double yInfEsq = Convert.ToDouble(linha.Substring(5, 5).Trim());
+                    Double xSupDir = Convert.ToDouble(linha.Substring(10, 5).Trim());
+                    Double ySupDir = Convert.ToDouble(linha.Substring(15, 5).Trim());
+
+                    while ((linha = arqFiguras.ReadLine()) != null)
                     {
-                        var figuraLida = new Ponto(0,0,Color.White);
+                        String tipo = linha.Substring(0, 5).Trim();
+                        int xBase = Convert.ToInt32(linha.Substring(5, 5).Trim());
+                        int yBase = Convert.ToInt32(linha.Substring(10, 5).Trim());
+                        int corR = Convert.ToInt32(linha.Substring(15, 5).Trim());
+                        int corG = Convert.ToInt32(linha.Substring(20, 5).Trim());
+                        int corB = Convert.ToInt32(linha.Substring(25, 5).Trim());
+                        Color cor = new Color();
+                        cor = Color.FromArgb(255, corR, corG, corB);
 
-                        string tipoFigura = linha.Substring(0, 5);
-
-                        switch (tipoFigura)
+                        switch (tipo[0])
                         {
-                            case "p":                                
-                                string coordBaseX = linha.Substring(6, 5);
-                                string coordBaseY = linha.Substring(11, 5);
-                                string corR = linha.Substring(16, 5);
-                                string corG = linha.Substring(21, 5);
-                                string corB = linha.Substring(26, 5);                                                                
-                                figuraLida = new Ponto(Convert.ToInt32(coordBaseX), Convert.ToInt32(coordBaseY), 
-                                                            Color.FromArgb(Convert.ToInt32(corR), Convert.ToInt32(corG), Convert.ToInt32(corB)));
+                            case 'p': // figura é um ponto
+                                figuras.InserirAposFim(
+                                    new NoLista<Ponto>(new Ponto(xBase, yBase, cor), null));
                                 break;
-
-                            case "l":
-                                coordBaseX = linha.Substring(6, 5);
-                                coordBaseY = linha.Substring(11, 5);
-                                corR = linha.Substring(16, 5);
-                                corG = linha.Substring(21, 5);
-                                corB = linha.Substring(26, 5);
-                                string fimX = linha.Substring(31, 5);
-                                string fimY = linha.Substring(36, 5);
-                                figuraLida = new Reta(Convert.ToInt32(coordBaseX), Convert.ToInt32(coordBaseY), Convert.ToInt32(fimX), Convert.ToInt32(fimY),
-                                                            Color.FromArgb(Convert.ToInt32(corR), Convert.ToInt32(corG), Convert.ToInt32(corB)));
+                            case 'l': // figura é uma reta
+                                int xFinal = Convert.ToInt32(linha.Substring(30, 5).Trim());
+                                int yFinal = Convert.ToInt32(linha.Substring(35, 5).Trim());
+                                figuras.InserirAposFim(new NoLista<Ponto>(
+                                        new Reta(xBase, yBase, xFinal, yFinal, cor), null));
                                 break;
-
-                            case "c":
-                                coordBaseX = linha.Substring(6, 5);
-                                coordBaseY = linha.Substring(11, 5);
-                                corR = linha.Substring(16, 5);
-                                corG = linha.Substring(21, 5);
-                                corB = linha.Substring(26, 5);
-                                string raio = linha.Substring(31, 5);
-                                figuraLida = new Circulo(Convert.ToInt32(coordBaseX), Convert.ToInt32(coordBaseY), Convert.ToInt32(raio),
-                                                            Color.FromArgb(Convert.ToInt32(corR), Convert.ToInt32(corG), Convert.ToInt32(corB)));
-
-                                break;
-
-                            case "e":
-                                coordBaseX = linha.Substring(6, 5);
-                                coordBaseY = linha.Substring(11, 5);
-                                corR = linha.Substring(16, 5);
-                                corG = linha.Substring(21, 5);
-                                corB = linha.Substring(26, 5);
-                                string raioA = linha.Substring(31, 5);
-                                string raioB = linha.Substring(36, 5);
-                                figuraLida = new Elipse(Convert.ToInt32(coordBaseX), Convert.ToInt32(coordBaseY), Convert.ToInt32(raioA), Convert.ToInt32(raioB),
-                                                            Color.FromArgb(Convert.ToInt32(corR), Convert.ToInt32(corG), Convert.ToInt32(corB)));
-
+                            case 'c': // figura é um círculo
+                                int raio = Convert.ToInt32(linha.Substring(30, 5).Trim());
+                                figuras.InserirAposFim(new NoLista<Ponto>(
+                                           new Circulo(xBase, yBase, raio, cor), null));
                                 break;
                         }
-
-                        figuras.InserirAposFim(figuraLida);
-                        
-                         
                     }
+                    arqFiguras.Close();
+                    this.Text = dlgAbrir.FileName;
+                    pbAreaDesenho.Invalidate();
                 }
+                catch (IOException)
+                {
+                    Console.WriteLine("Erro de leitura no arquivo");
+                }
+        }
+        
+
+        private void pbAreaDesenho_MouseMove(object sender, MouseEventArgs e)
+        {
+            stMensagem.Items[3].Text = e.X + "," + e.Y;
+        }
+
+        private void btnPonto_Click(object sender, EventArgs e)
+        {
+            stMensagem.Items[1].Text = "clique no local do ponto desejado:";
+            limparEsperas();
+            esperaPonto = true;
+
+        }
+
+        private void pbAreaDesenho_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (esperaPonto)
+            {
+                Ponto novoPonto = new Ponto(e.X, e.Y, corAtual);
+                figuras.InserirAposFim(new NoLista<Ponto>(novoPonto, null));
+                novoPonto.Desenhar(novoPonto.Cor, pbAreaDesenho.CreateGraphics());
+                esperaPonto = false;
+                stMensagem.Items[1].Text = "";
             }
+            else
+                if (esperaInicioReta)
+                 {
+                     p1.Cor = corAtual;
+                     p1.X = e.X;
+                     p1.Y = e.Y;
+                     esperaInicioReta = false;
+                     esperaFimReta = true;
+                     stMensagem.Items[1].Text = "Mensagem: clique o ponto final da reta";
+            }
+            else
+                  if (esperaFimReta)
+                  {
+                     esperaInicioReta = false;
+                     esperaFimReta = false;
+                     Reta novaLinha = new Reta(p1.X, p1.Y, e.X, e.Y, corAtual);
+                     figuras.InserirAposFim(new NoLista<Ponto>(novaLinha, null));
+                     novaLinha.Desenhar(novaLinha.Cor, pbAreaDesenho.CreateGraphics());
+                    }
+
+
+
+        }
+
+        private void btnReta_Click(object sender, EventArgs e)
+        {
+            stMensagem.Items[1].Text = "Clique no local do ponto inicial da reta:";
+            limparEsperas();
+            esperaInicioReta = true;
+
         }
     }
+
+    
+
 }
+
+
