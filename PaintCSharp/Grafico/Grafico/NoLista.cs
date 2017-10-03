af337594ff38603ce8044a8ac537e807cf0bb3c5
CAMILA OMAE <milaomae@gmail.com>
2017-10-02 22:10:07 -0300

    finalizacoes das instrucoes

--- PaintCSharp/Grafico/Grafico/NoLista.cs
+++ PaintCSharp/Grafico/Grafico/NoLista.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 namespace Grafico
 {
     public class NoLista<Dado> where Dado : IComparable<Dado>
