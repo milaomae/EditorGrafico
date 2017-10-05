46ed3b6cd3cd21116ee5c168b8aa71fecf45291d
milaomae <31636124+milaomae@users.noreply.github.com>
2017-10-04 22:57:59 -0300

    Update Reta.cs

--- PaintCSharp/Grafico/Grafico/Reta.cs
+++ PaintCSharp/Grafico/Grafico/Reta.cs
@@ -27,23 +27,23 @@ namespace Grafico
                             pontoFinal.X, pontoFinal.Y);
         }
 
-        public int CompareTo(Reta other)
-        {
-            //precisa implementar
-            return 0;
-        }
+      //  public int CompareTo(Reta other)
+      //  {
+       //     //precisa implementar
+       //     return 0;
+      //  }
         
-        public override String ToString()
-        {
-            return transformaString("l", 5) +
-                  transformaString(base.X, 5) +
-                  transformaString(base.Y, 5) +
-                  transformaString(pontoFinal.X, 5) +
-                  transformaString(pontoFinal.Y, 5) +
-                    transformaString(Cor.R, 5) +
-                    transformaString(Cor.G, 5) +
-                    transformaString(Cor.B, 5);
-        }
+     //   public override String ToString()
+     //   {
+     //       return transformaString("l", 5) +
+    //              transformaString(base.X, 5) +
+     //             transformaString(base.Y, 5) +
+     //             transformaString(pontoFinal.X, 5) +
+     //             transformaString(pontoFinal.Y, 5) +
+     //               transformaString(Cor.R, 5) +
+     //               transformaString(Cor.G, 5) +
+    //                transformaString(Cor.B, 5);
+     //   }
 
     }
 }
